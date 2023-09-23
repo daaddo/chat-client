3d504e61b6456a8d054194afe376408382a03980
DaviDio <david.cascella.5@gmail.com>
2023-09-23 20:17:26 +0200

    GUI first commit
    Logger Manager for info, debug, warning and errors

--- src/main/java/it/cascella/chat/Main.java
+++ src/main/java/it/cascella/chat/Main.java
@@ -1,7 +1,32 @@
 package it.cascella.chat;
 
+import it.cascella.chat.gui.login.LoginFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(LoginFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
 }
---
+++ src/main/java/it/cascella/chat/logger/LoggerManager.java
@@ -0,0 +1,53 @@
+package it.cascella.chat.logger;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class LoggerManager {
+    private static LoggerManager instance = null;
+    private boolean verbose = true;
+
+    private LoggerManager() {
+    }
+
+    public static LoggerManager getInstance() {
+        if (instance == null) {
+            instance = new LoggerManager();
+        }
+        return instance;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    private String now() {
+        Date now = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        return simpleDateFormat.format(now);
+    }
+
+    public void info(String string) {
+        System.out.println(now() + " [INFO] " + string);
+    }
+    public void warn(String string) {
+        System.out.println(now() + " [WARNING] " + string);
+    }
+    public void error(String string) {
+        System.out.println(now() + " [ERROR] " + string);
+    }
+    public void debug(String string) {
+        if (verbose) {
+            System.out.println(now() + " [DEBUG] " + string);
+        }
+    }
+
+
+}
